1f31a1d15cb44d2d9d32582ae55aaba672c52cf2
nasrin <n.mosh4yedi@gmail.com>
2019-04-30 02:13:10 +0430

    [add] add menu

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import MainPage from "./imports/ui/components/template/mainPage/mainPage";
+import MainPage from "./imports/ui/components/template/mainPage/MainPage";
+import "./css/AppStyle.css";
 function App() {
-  return (
-    <div className="App">
-      <MainPage />
-    </div>
-  );
+  return <div className="App">
+    <MainPage />
+  </div>;
 }
 
 export default App;
--- spa-react/src/imports/ui/components/template/mainPage/MainPage.js
+++ spa-react/src/imports/ui/components/template/mainPage/MainPage.js
@@ -1,32 +1,31 @@
 import React from "react";
-// import { withStyles } from "@material-ui/core/styles";
+
 // material-ui imports
 import MenuItem from "@material-ui/core/MenuItem";
 import Grid from "@material-ui/core/Grid";
 
-// style
-// import "mainPage.css";
 
 function MainPage() {
   return (
-    <div>
+    <div className="red">
+
       <Grid
         container
-        justify="center"
+        justify="flex-end"
         alignContent="space-around"
         alignItems="center"
       >
         <Grid item xs={1}>
-          <MenuItem style={{ textAlign: "center" }}>home</MenuItem>
+          <MenuItem class="center-menu-item">home</MenuItem>
         </Grid>
         <Grid item xs={1}>
-          <MenuItem>about me</MenuItem>
+          <MenuItem class="center-menu-item">about me</MenuItem>
         </Grid>
         <Grid item xs={1}>
-          <MenuItem>contact me</MenuItem>
+          <MenuItem class="center-menu-item">contact me</MenuItem>
         </Grid>
         <Grid item xs={1}>
-          <MenuItem>skills</MenuItem>
+          <MenuItem class="center-menu-item">skills</MenuItem>
         </Grid>
       </Grid>
     </div>
